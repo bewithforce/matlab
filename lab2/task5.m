3dcad7e6253fae37893acb035f8cfcb5d73f868c
dzianis <denis.prist.poryvay@gmail.com>
2019-12-06 16:19:45 +0300

    lab2 task5

--- lab2/task5.m
+++ lab2/task5.m
@@ -5,16 +5,23 @@ y_arg = zeros(length(x));
 a = rand(size(x));
 b = rand(size(x));
 
+x = -20:20;
+y = zeros(length(x));
+buff1 = 0;
+buff2 = 0;
+
 for i=1:length(x)
-    x(i) = i * pi/length(x);
-    y(i) = H_func(x, a, b);
+    x(i) = H_func(x, a, b);
+    y(i) = x(i) + buff1 + buff2;
+    buff1 = x(i);
+    buff2 = x(i) + y(i);
     y_abs(i) = abs(y(i));
     y_arg(i) = angle(y(i));
-end  
+end
 
 figure;
 subplot(2,1,1);
-stem(y_abs);
+plot(y_abs);
 subplot(2,1,2);
 stem(y_arg);
 
@@ -22,11 +29,11 @@ stem(y_arg);
 function y = H_func(omega, a, b)
     den = 1;
     y = 0;
-    for i = 1:length(b)
-        y = y + b(i) * (cos(omega * i) + i*sin(omega*i));
+    for k = 1:length(b)
+        y = y + b(k) * (cos(omega * k) + i*sin(omega*k));
     end
-    for i = 2:length(a)
-        den = den + a(i) * (cos(omega * i) + i*sin(omega*i));
+    for k = 2:length(a)
+        den = den + a(k) * (cos(omega * k) + i*sin(omega*k));
     end
     y = y/den;
 end
